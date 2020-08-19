d36d19b376d236e99c4bfaa458c6b28ed1fa9346
obiPlabon <obi.plabon@gmail.com>
2020-08-19 07:03:42 +0600

    js functionality done

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,38 +1,65 @@
 ;
 (function ($) {
-    'use strict';
+	'use strict';
 
-    $(function () {
-        var $picker = $('.funnel-picker'),
-            $picker_options = $picker.find('.funnel-picker__options-input'),
-            $package = $picker.find('#package'),
-            $form = $picker.find('.variations_form'),
-            variation_data = $form.data('product_variations');
+	$(function () {
+		var $picker = $('.funnel-picker'),
+			$picker_options = $picker.find('.funnel-picker__options-input'),
+			$package = $picker.find('#package'),
+			$form = $picker.find('.variations_form'),
+			variation_data = $form.data('product_variations'),
+			$price_base = $picker.find('.funnel-picker__top-price-base'),
+			$price_bottle = $picker.find('.funnel-picker__top-price-bottle');
 
-        $picker_options.on('change', function () {
-            $package
-                .find('option[value="' + this.value + '"]')
-                .attr('selected', true)
-                .trigger('change')
-                .siblings()
-                .attr('selected', false);
-        });
+		function updatePriceTitle() {
+			var variation_id = $picker_options.closest(':checked').data('vid'),
+				type = $picker.find('.wcsatt-options-prompt-action-input:checked').val(),
+				bottle_price_type = type === 'yes' ? 'funnel_subs_bottle_price' : 'funnel_ot_bottle_price',
+				price_type = type === 'yes' ? 'subscription' : 'onetime',
+				$base_price_markup = $picker.find('.funnel-' + price_type + '-price-radio.funnel-product-' + variation_id);
 
-        $picker.on('change.onPromptAction', '.wcsatt-options-prompt-action-input', function () {
-            var bottle_price_type = 'funnel_ot_bottle_price';
-            // Subscription
-            if (this.value === 'yes') {
-                bottle_price_type = 'funnel_subs_bottle_price';
-            }
+			$.each(variation_data, function (index, item) {
+				if (item.variation_id !== variation_id) {
+					return;
+				}
+				$price_bottle.html('(' + item[bottle_price_type] + ')');
+			});
 
-            $.each(variation_data, function (index, item) {
-                var $bottle_price = $picker
-                    .find('.funnel-picker__options-input[data-vid="' + item.variation_id + '"]')
-                    .parents('.funnel-picker__options-item')
-                    .find('.funnel-picker__options-price');
+			if (type === 'yes') {
+				$price_base.html($base_price_markup.find(':first-child').clone().html());
+			} else {
+				$price_base.html($base_price_markup.html());
+			}
+		}
 
-                $bottle_price.html(item[bottle_price_type]);
-            });
-        });
-    });
+		$picker_options.on('change.onPackageChage', function () {
+			$package
+				.find('option[value="' + this.value + '"]')
+				.attr('selected', true)
+				.trigger('change')
+				.siblings()
+				.attr('selected', false);
+
+			updatePriceTitle();
+		});
+
+		$picker.on('change.onPromptActionChange', '.wcsatt-options-prompt-action-input', function () {
+			var bottle_price_type = 'funnel_ot_bottle_price';
+			// Subscription
+			if (this.value === 'yes') {
+				bottle_price_type = 'funnel_subs_bottle_price';
+			}
+
+			$.each(variation_data, function (index, item) {
+				var $bottle_price = $picker
+					.find('.funnel-picker__options-input[data-vid="' + item.variation_id + '"]')
+					.parents('.funnel-picker__options-item')
+					.find('.funnel-picker__options-price');
+
+				$bottle_price.html(item[bottle_price_type]);
+			});
+
+			updatePriceTitle();
+		});
+	});
 }(jQuery));
